f7945b6ff667969801bf94048ea779fd891b0cc3
Henry Jung <butwoo91@gmail.com>
2025-12-10 18:55:04 +1100

    reload the entire project due to previous issues on optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/*.liquid'
-  ],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      height: {
+        '94': '22rem'
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
